d5196c4c070d9ecd2809a685e43dc932bcd604cd
joaobzao <jmmbzao@gmail.com>
2025-12-05 12:33:22 +0000

    update capas version

--- capas-app/composeApp/build.gradle.kts
+++ capas-app/composeApp/build.gradle.kts
@@ -46,7 +46,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 2
-        versionName = "1.0.1"
+        versionName = "1.0.2"
     }
     packaging {
         resources {
