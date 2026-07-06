62a2bbce1a0ea515bf40071fe1883f1173fd694b
joaobzao <8434594+joaobzao@users.noreply.github.com>
2026-07-06 10:45:09 +0000

    chore: bump android version to 1.2.0

--- capas-app/composeApp/build.gradle.kts
+++ capas-app/composeApp/build.gradle.kts
@@ -48,7 +48,7 @@ android {
         applicationId = "com.joaobzao.capas"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 25
+        versionCode = 26
         versionName = "1.2.0"
         ndk {
             debugSymbolLevel = "FULL"
